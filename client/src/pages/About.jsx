713b778c65d044333f246afd455d31d3ffaf480e
panku-chavan <pankajchavan963@gmail.com>
2023-03-27 15:02:04 +0530

    privacy policy page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <div className="row contactus ">
                 <div className="col-md-6 ">
                     <img
-                        src="/images/about.jpeg"
+                        src="/images/about.jpg"
                         alt="contactus"
                         style={{ width: "100%" }}
                     />
--- client/src/pages/Policy.jsx
+++ client/src/pages/Policy.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from "react";
+import Layout from "./../components/Layout/Layout";
 
 const Policy = () => {
     return (
-        <Layout>
-            <h1>Policy Page</h1>
+        <Layout title={"Privacy Policy"}>
+            <div className="row contactus ">
+                <div className="col-md-6 ">
+                    <img
+                        src="/images/contactus.jpeg"
+                        alt="contactus"
+                        style={{ width: "100%" }}
+                    />
+                </div>
+                <div className="col-md-4">
+                    <p>add privacy policy</p>
+                    <p>add privacy policy</p>
+                    <p>add privacy policy</p>
+                    <p>add privacy policy</p>
+                    <p>add privacy policy</p>
+                    <p>add privacy policy</p>
+                    <p>add privacy policy</p>
+                </div>
+            </div>
         </Layout>
-    )
-}
+    );
+};
 
-export default Policy
+export default Policy;

12284d00e47309b77e931bed49cbd8e782de5bb0
panku-chavan <pankajchavan963@gmail.com>
2023-03-27 14:12:07 +0530

    navbar complete

--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -1,36 +1,65 @@
-import React from 'react'
-import { NavLink } from "react-router-dom";
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { AiFillShopping } from "react-icons/ai"
 
 const Header = () => {
     return (
         <>
-
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <NavLink to="/" className="navbar-brand" href="#">Navbar</NavLink>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <Link to="/" className="navbar-brand">
+                        🛒E-Commerce
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarTogglerDemo02"
+                        aria-controls="navbarTogglerDemo02"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink to="/" className="nav-link active" aria-current="page" href="#">Home</NavLink>
+                                <NavLink
+                                    to="/"
+                                    className="nav-link"
+                                >
+                                    Home
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/" className="nav-link" href="#">Link</NavLink>
+                                <NavLink
+                                    to="/category"
+                                    className="nav-link"
+                                >
+                                    Category
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <NavLink to="/" className="nav-link disabled">Disabled</NavLink>
+                                <NavLink to="/register" className="nav-link">
+                                    Register
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/login" className="nav-link" >
+                                    Login
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/cart" className="nav-link" >
+                                    Cart (0)
+                                </NavLink>
                             </li>
                         </ul>
-
                     </div>
                 </div>
             </nav>
-
-
         </>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;

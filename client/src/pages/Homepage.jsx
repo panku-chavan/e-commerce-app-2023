2cdac05bf91581bf8725fe2bbdbafa6abbd4fac0
panku-chavan <pankajchavan963@gmail.com>
2023-04-02 14:01:11 +0530

    Filter by categories home page

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,57 +1,104 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../components/Layout/Layout";
-import { useAuth } from "../Context/AuthContext";
+// import { useAuth } from "../Context/AuthContext";
 import toast from "react-hot-toast";
 import axios from "axios";
+import { Checkbox } from "antd";
 
 const Homepage = () => {
-    const [auth] = useAuth();
-    const [products, setProducts] = useState([]);
-    const [categories, setCategories] = useState([]);
+  // const [auth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
 
-    // get products
+  //get categories
 
-    const getProducts = async () => {
-        try {
-            const respons = await axios.get("/api/v1/products/get-product");
-            setProducts(respons.data.products);
-            // console.log(respons.data.products)
-        } catch (error) {
-            console.log(error);
-            toast.error("Something Went Wrong");
-        }
-    };
-    useEffect(() => {
-        getProducts();
-    }, []);
-    return (
-        <Layout title={"All Products - Best Offers"}>
-            <div className="row mt-3">
-                <div className="col-md-3 ">
-                    <h4 className="text-center">Filter By Category</h4>
+  // get all category
+  const getAllCategory = async () => {
+    try {
+      const response = await axios.get("/api/v1/category/get-category");
+      if (response.data?.success) {
+        setCategories(response.data.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getAllCategory();
+  }, []);
+  // get products
+
+  const getProducts = async () => {
+    try {
+      const respons = await axios.get("/api/v1/products/get-product");
+      setProducts(respons.data.products);
+      // console.log(respons.data.products)
+    } catch (error) {
+      console.log(error);
+      toast.error("Something Went Wrong");
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const handleFilter = (value, id) => {
+    // console.log(value, id)
+    let all = [...checked];
+    if (value) {
+      all.push(id)
+    } else {
+      all = all.filter((c) => c !== id)
+    }
+    setChecked(all);
+  };
+
+  return (
+    <Layout title={"All Products - Best Offers"}>
+      <div className="row mt-3">
+        <div className="col-md-2 ">
+          <h4 className="text-center mt-2">Filter By Category</h4>
+          <div className="d-flex flex-column ">
+            {categories?.map((c) => (
+              <Checkbox
+                className="mt-2"
+                key={c._id}
+                onChange={(e) => handleFilter(e.target.checked, c._id)}
+              >
+                {c.name}
+              </Checkbox>
+            ))}
+          </div>
+        </div>
+        <div className="col-md-9">
+          {JSON.stringify(checked, null, 4)}
+          <h2 className="text-center">All Products</h2>
+          <div className="d-flex flex-wrap">
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }} key={p._id}>
+                <img
+                  src={`/api/v1/products/product-image/${p._id}`}
+                  className="card-img-top"
+                  alt={p.name}
+                  height={"300px"}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description}</p>
+                  <p className="card-text">Rs. {p.price} </p>
+                  <button className="btn btn-primary ms-2">See Details</button>
+                  <button className="btn btn-secondary ms-2">
+                    Add To Cart
+                  </button>
                 </div>
-                <div className="col-md-9">
-                    <h2 className="text-center">All Products</h2>
-                    <div className="d-flex flex-wrap">
-                        {products.map((p) => (
-                            <div className="card m-2" style={{ width: "18rem" }} key={p._id}>
-                                <img
-                                    src={`/api/v1/products/product-image/${p._id}`}
-                                    className="card-img-top"
-                                    alt={p.name}
-                                    height={'300px'}
-                                />
-                                <div className="card-body">
-                                    <h5 className="card-title">{p.name}</h5>
-                                    <p className="card-text">{p.description}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-        </Layout>
-    );
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Homepage;
